1d9aac8279c80fb65651075114b4cefd1821e20d
sazidhayatkhan <sazidhayatkhan@gmail.com>
2022-08-21 18:10:30 +0600

    'More' page completed

--- components/TrendingNow.tsx
+++ components/TrendingNow.tsx
@@ -7,7 +7,7 @@ const TrendingNow = () => {
   return (
     <div>
     <div className='grid grid-cols-2 justify-between items-center ml-4 whitespace-nowrap mt-4 mb-4'>
-        <h1 className='text-xl font-bold text-white'>Tranding Now</h1>
+        <h1 className='text-xl font-bold text-white'>Trending Now</h1>
         <p className=' text-white font-thin ml-auto mr-8'>View all</p>
     </div>
     <div>
---
+++ pages/More.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faUserTie, faArrowLeft, faPenToSquare, faGift, faStar, faGear, faAngleRight} from '@fortawesome/free-solid-svg-icons'
+
+const More = () => {
+  return (
+    <div className='bg-slate-900 min-h-screen font-body text-white'>
+        <div className='grid grid-cols-2 ml-4 pt-4 mb-4 justify-items-stretch'>
+            <div className='text-2xl text-white flex space-x-3 items-center'>
+                <FontAwesomeIcon icon={faArrowLeft}/> 
+                <h1 className='font-light'>More</h1> 
+            </div>
+            <div className=' text-white font-thin mr-4 ml-auto text-2xl relative'>
+                <FontAwesomeIcon icon={faPenToSquare}/>   
+            </div>
+        </div>
+        <div className='grid place-items-center'>
+            <div className='bg-cyan-400 rounded-full p-6 px-7 ring-2 ring-white'>
+              <FontAwesomeIcon icon={faUserTie}/>  
+            </div>
+            <div className='mt-3 grid place-items-center'>
+                <h1>Ishtiaq Ahmed</h1>
+                <p>01XX-XXX-XX</p>
+            </div>           
+        </div>
+        <div className='flex justify-center mt-4'>
+            <h1>-------------</h1>
+        </div>
+
+        <div className='flex justify-center mt-8'>
+            <FontAwesomeIcon icon={faGift} className='bg-purple-600 p-4'/>
+            <button className='bg-purple-600 pr-6'>Collect Bonus</button>
+        </div>
+        <div>
+        <div className='bg-slate-700 mx-6 mt-16 rounded-xl'>
+                            <div className='flex text-white py-4 ml-4'>
+                                    <div className='text-sm'>
+                                        <p>Premium package</p>
+                                        <p className=' text-slate-300'>30 Days</p>
+                                    </div>
+                                    <h1 className='font-bold ml-auto mr-12 text-center mt-2 text-xl'>39 BDT</h1>
+                            </div>
+                        </div>   
+        </div>
+
+
+    <div className='mx-6 my-4'>
+        <div className='flex items-center py-8'>
+            <div className='flex items-center space-x-3'>
+                <FontAwesomeIcon icon={faStar} className='bg-white text-slate-500 p-2 rounded-xl'/> 
+                <h1>FAQ</h1>
+            </div>
+            <FontAwesomeIcon icon={faAngleRight} className='text-white ml-auto'/>
+        </div>       
+        <div className='flex items-center'>
+            <div className='flex items-center space-x-3'>
+                <FontAwesomeIcon icon={faGear} className='bg-white text-slate-500 p-2 rounded-xl'/> 
+                <h1>Settings</h1>
+            </div>
+            <FontAwesomeIcon icon={faAngleRight} className='text-white ml-auto'/>
+        </div>
+    </div>    
+
+
+
+        <div className='mt-28 pb-6 flex justify-center'>
+            <h1>Unsubscribe</h1>
+        </div>
+    </div>
+  )
+}
+
+export default More
