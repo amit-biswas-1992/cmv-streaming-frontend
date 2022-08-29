8a76a738bd3e8b47872636398d1560f986773fbe
sazidhayatkhan <sazidhayatkhan@gmail.com>
2022-08-29 14:25:03 +0600

    uploaded

--- components/Corasoul.tsx
+++ components/Corasoul.tsx
@@ -6,16 +6,15 @@ import Image from 'next/image'
 
 const CardNewRelease = (props) => {
   return (
-    <div className='flex space-x-4 overflow-x-auto scrollbar-hide overscroll-x-contain mx-4 cursor-pointer mt-4'>
+    <div className='flex space-x-4 overflow-x-auto scrollbar-hide overscroll-x-contain ml-4 cursor-pointer mt-4 mb-8'>
     {props.details.map( (value,index)=>(
 
      <div className='' key={index}>
 
           
        
-        <Image className='' src={value.img} alt='' />
-              <div className='w-32 whitespace-nowrap text-white items-center align-middle mr-32'>
-                <h1 className='text-slate-900'>!!!!!!!!!!!!!!</h1>
+        <Image className='rounded-xl' src={value.img} alt='' />
+              <div className='w-32 whitespace-nowrap text-white items-center align-middle mr-32'>             
               </div>   
     </div>
     ))}
