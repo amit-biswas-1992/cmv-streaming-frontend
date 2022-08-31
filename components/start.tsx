16b7b6965501b1a35cba055f8311ce52a3cb9079
sazidhayatkhan <sazidhayatkhan@gmail.com>
2022-08-31 16:20:53 +0600

    fixed

--- components/start.tsx
+++ components/start.tsx
@@ -28,14 +28,14 @@ const Start = () => {
                         <span className="text-blue-400">CMV DRAMA</span>
                         </h3>
                     </div>
-                    <div className="grid place-items-center px-8">
+                    <div className="grid fixed bottom-20 inset-x-0 z-50 place-items-center px-8">
                         <Link href='phone'>
                         <button className="rounded-2xl w-full py-4 my-4 mt-8 bg-sky-600 hover:bg-sky-700 text-white text-center">
                         Registration
                         </button>
                         </Link>
                         <Link href='home'>
-                        <button className="pt-4 mt-4 grid place-items-center text-center">Log In</button>
+                        <button className="mt-2 grid place-items-center text-center">Log In</button>
                         </Link>
                     </div>
                 </div>
