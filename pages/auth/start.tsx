227a132bc0add6e8909c715724916414c5eae703
sazidhayatkhan <sazidhayatkhan@gmail.com>
2022-09-08 14:24:38 +0600

    added

--- pages/auth/start.tsx
+++ pages/auth/start.tsx
@@ -2,17 +2,16 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Logo from '../../components/assets/CMV.png'
-import BG from '../../components/assets/bg.jpg'
+
+import style from '../../components/start.module.css'
 
 
 const Start = () => {
 
     
   return (
-    <div className="">
-      <div className="w-full h-full bg-slate-900/80 absolute font-body">
-
-      </div>
+    <div className={style.item}>
+      
       
 
       <div className="text-white relative">
@@ -22,15 +21,15 @@ const Start = () => {
                 </div>
 
             
-              <div className="mt-44">
+              <div className="pt-44">
                 <div className=''>
-                    <div className="px-4">
+                     <div className="px-4">
                         <h3 className="text-lg px-5 text-center">
                         First time in Bangladesh Unlimited Drama only on{" "}
                         <span className="text-blue-400">CMV DRAMA</span>
                         </h3>
-                    </div>
-                    <div className="grid fixed bottom-20 inset-x-0 z-50 place-items-center px-8">
+                    </div> 
+                     <div className="grid fixed bottom-20 inset-x-0 z-50 place-items-center px-8">
                         <Link href='../auth/register'>
                         <button className="rounded-2xl w-full py-4 my-4 mt-8 bg-sky-600 hover:bg-sky-700 text-white text-center">
                         Registration
@@ -39,7 +38,7 @@ const Start = () => {
                         <Link href='../auth/login'>
                         <button className="mt-2 grid place-items-center text-center">Log In</button>
                         </Link>
-                    </div>
+                    </div> 
                 </div>
               </div> 
 
