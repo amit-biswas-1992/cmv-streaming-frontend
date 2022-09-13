7dcfc5f34cb35614b7087758b6e9148d61ba5d24
Ayon Al Mamun <ayonalmamun@gmail.com>
2022-09-13 12:29:04 +0600

    added missing div in tag-wise media

--- pages/tag-wise-media/[id].tsx
+++ pages/tag-wise-media/[id].tsx
@@ -44,59 +44,59 @@ const ViewComp = ({ data }) => {
             <FontAwesomeIcon icon={faArrowLeft} />
           </Link>
         </div>
-         
-                        
-                      
-          <div className='grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 px-4'>
-                       {
-                         data.map((curElem)=>{
-                          return(                                                        
-                                <div className='' key={curElem.id}>
-                                  <Link href={`/content-original/preview-new/${curElem.id}`}><a>
-                                  <Image className='rounded-2xl'loader={myLoader}
-                  src={curElem.cover} width={300} height={350}  alt=''/></a>
-                                  </Link> 
-                                    <div className='whitespace-nowrap text-white items-center align-middle'>
-                                      <h1 className='text-white'>{curElem.title}</h1>
-                                      {/* <p className='text-cyan-400'>{curElem.category}</p> */}
-                                    </div>     
-                                </div> 
-                                                          
-                            )
-                            
-                        }
-                        )
-                      }
-          </div>
-          <div className='pb-24'>
-          </div>           
-                        
 
-      <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 px-4">
-        {data.map((curElem) => {
-          return (
-            <div className="" key={curElem.id}>
-              <Link href={`/content-original/preview-new/${curElem.id}`}>
-                <a>
-                  <Image
-                    className="rounded-2xl"
-                    loader={myLoader}
-                    src={curElem.cover}
-                    width={300}
-                    height={350}
-                    alt=""
-                  />
-                </a>
-              </Link>
-              <div className="whitespace-nowrap text-white items-center align-middle">
-                <h1 className="text-white">{curElem.title}</h1>
-                {/* <p className='text-cyan-400'>{curElem.category}</p> */}
+        <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 px-4">
+          {data.map((curElem) => {
+            return (
+              <div className="" key={curElem.id}>
+                <Link href={`/content-original/preview-new/${curElem.id}`}>
+                  <a>
+                    <Image
+                      className="rounded-2xl"
+                      loader={myLoader}
+                      src={curElem.cover}
+                      width={300}
+                      height={350}
+                      alt=""
+                    />
+                  </a>
+                </Link>
+                <div className="whitespace-nowrap text-white items-center align-middle">
+                  <h1 className="text-white">{curElem.title}</h1>
+                  {/* <p className='text-cyan-400'>{curElem.category}</p> */}
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
+        <div className="pb-24"></div>
+
+        <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 px-4">
+          {data.map((curElem) => {
+            return (
+              <div className="" key={curElem.id}>
+                <Link href={`/content-original/preview-new/${curElem.id}`}>
+                  <a>
+                    <Image
+                      className="rounded-2xl"
+                      loader={myLoader}
+                      src={curElem.cover}
+                      width={300}
+                      height={350}
+                      alt=""
+                    />
+                  </a>
+                </Link>
+                <div className="whitespace-nowrap text-white items-center align-middle">
+                  <h1 className="text-white">{curElem.title}</h1>
+                  {/* <p className='text-cyan-400'>{curElem.category}</p> */}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="pb-24"></div>
       </div>
-      <div className="pb-24"></div>
     </div>
   );
 };
